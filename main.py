6f8518a9b371be157f10d1981f0e1aa0e294f656
Jukka Kansanaho <jukka.kansanaho@gmail.com>
2025-06-13 07:01:31 +0300

    feat: add list to handle user prompt history

--- main.py
+++ main.py
@@ -3,14 +3,27 @@ import sys
 #import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-contents = ""
+# Prepare the client
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+user_prompt = ""
+
+# Handle arguments
 if len(sys.argv) < 2:
     print("Error: no arguments. Please provide ai-agent prompt as the first argument.")
     sys.exit(1)
 else:
-    contents = sys.argv[1]
-    print(f"Prompt: {contents}")
+    user_prompt = sys.argv[1]
+    print(f"Prompt: {user_prompt}")
+
+# Prepare the messages
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+]
+
 '''
 # Alternative way to handle arguments with argparse
 #
@@ -24,44 +37,16 @@ parser.add_argument(
 args = parser.parse_args()
 contents = args.prompt
 '''
+# Main function
 def main():
-    load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
-    client = genai.Client(api_key=api_key)
-
-    '''
-    Use the client.models.generate_content() method to get a response from the gemini-2.0-flash-001 model! You'll need to use two named parameters:
-    model: The model name: gemini-2.0-flash-001 (this one has a generous free tier)
-    contents: The prompt to send to the model (a string). Use this prompt:
-    "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-
-    In addition to printing the text response, print the number of tokens consumed by the interaction in this format:
-    Prompt tokens: X
-    Response tokens: Y
-    The response has a .usage_metadata property that has both:
-
-    a prompt_token_count property (tokens in the prompt)
-    a candidates_token_count property (tokens in the response)
-    '''
+    # Generate the response
     model = "gemini-2.0-flash-001"
-    #contents = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model=model, contents=contents)
-
+    response = client.models.generate_content(model=model, contents=messages)
+    # Print the response
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(f"Total token count: {response.usage_metadata.total_token_count}")
-
-    '''
-    Response:
-    Boot.dev excels as a backend development learning platform due to its hands-on, project-based approach. It emphasizes practical coding from the outset, allowing
-    learners to build real-world applications while mastering core concepts through interactive courses and immediate feedback. With structured paths and a focus on
-    industry-relevant skills like Go and Python, Boot.dev effectively bridges the gap between theory and practice, making complex topics accessible and preparing stu
-    dents for successful careers.
-
-    Prompt tokens: 19
-    Response tokens: 89
-    '''
 
 if __name__ == "__main__":
     main()
